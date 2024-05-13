015016b19c48e150294c846885239ce6d94495bd
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-05-13 17:38:42 -0300

    feat: add logics to create, complete and delete a task

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,82 @@
 import { PlusCircle } from "@phosphor-icons/react";
 import { Header } from "./components/header/header";
 import clipboard from "./assets/clipboard.svg";
-
+import { v4 as uuidv4 } from "uuid";
 import styles from "./app.module.css";
 import { Task } from "./components/task/task";
+import { useState } from "react";
+import { ITask } from "./types/tasks";
 
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>(() =>
+    JSON.parse(localStorage.getItem("@ToDoList:taskList") || "[]")
+  );
+  const [newTask, setNewTask] = useState("");
+
+  const handleWriteNewTask = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTask(event.target.value);
+  };
+
+  const handleCreateTask = () => {
+    const newTaskList = [
+      ...taskList,
+      {
+        id: uuidv4(),
+        description: newTask,
+        isCompleted: false,
+      },
+    ];
+
+    localStorage.setItem("@ToDoList:taskList", JSON.stringify(newTaskList));
+
+    setTaskList(newTaskList);
+
+    setNewTask("");
+  };
+
+  const completeTask = (id: string, isChecked: boolean) => {
+    const task = taskList.find((task) => task.id === id);
+
+    if (!task) {
+      return;
+    }
+
+    const taskUpdated = {
+      ...task,
+      isCompleted: isChecked,
+    };
+
+    const taskListUpdated = taskList.map((task) => {
+      if (task.id === id) {
+        return taskUpdated;
+      }
+
+      return task;
+    });
+
+    localStorage.setItem("@ToDoList:taskList", JSON.stringify(taskListUpdated));
+
+    setTaskList(taskListUpdated);
+  };
+
+  const deleteTask = (id: string) => {
+    const taskListUpdated = taskList.filter((task) => task.id !== id);
+
+    localStorage.setItem("@ToDoList:taskList", JSON.stringify(taskListUpdated));
+
+    setTaskList(taskListUpdated);
+  };
+
+  const totalTasksCount = taskList.length;
+
+  const completedTasksCount = taskList.reduce((total, task) => {
+    if (task.isCompleted) {
+      return total + 1;
+    }
+
+    return total;
+  }, 0);
+
   return (
     <>
       <Header />
@@ -16,9 +87,11 @@ function App() {
             className={styles.newTaskInput}
             type="text"
             placeholder="Adicione uma nova tarefa"
+            value={newTask}
+            onChange={handleWriteNewTask}
           />
 
-          <button className={styles.newTaskButton}>
+          <button className={styles.newTaskButton} onClick={handleCreateTask}>
             Criar
             <PlusCircle size={16} />
           </button>
@@ -29,30 +102,40 @@ function App() {
             <div className={styles.count}>
               <p className={styles.createdTasks}>Tarefas criadas</p>
 
-              <span>0</span>
+              <span>{totalTasksCount}</span>
             </div>
 
             <div className={styles.count}>
               <p className={styles.completedTasks}>Concluídas</p>
 
-              <span>0</span>
+              <span>
+                {completedTasksCount} de {totalTasksCount}
+              </span>
             </div>
           </header>
 
           <div className={styles.taskList}>
-            <Task />
-            <Task />
+            {taskList.map((task) => (
+              <Task
+                key={task.id}
+                task={task}
+                onCompleteTask={completeTask}
+                onDeleteTask={deleteTask}
+              />
+            ))}
 
-            <div className={styles.noTask}>
-              <div className={styles.noTaskContent}>
-                <img src={clipboard} alt="Desenho de prancheta" />
+            {taskList.length === 0 && (
+              <div className={styles.noTask}>
+                <div className={styles.noTaskContent}>
+                  <img src={clipboard} alt="Desenho de prancheta" />
 
-                <div className={styles.noTaskText}>
-                  <p>Você ainda não tem tarefas cadastradas</p>
-                  <p>Crie tarefas e organize seus itens a fazer</p>
+                  <div className={styles.noTaskText}>
+                    <p>Você ainda não tem tarefas cadastradas</p>
+                    <p>Crie tarefas e organize seus itens a fazer</p>
+                  </div>
                 </div>
               </div>
-            </div>
+            )}
           </div>
         </div>
       </main>
--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,12 +1,25 @@
 import { Trash, Check } from "@phosphor-icons/react";
 import styles from "./task.module.css";
 import { useState } from "react";
+import { ITask } from "../../types/tasks";
 
-export const Task = () => {
-  const [isChecked, setIsChecked] = useState(false);
+interface TaskProps {
+  task: ITask;
+  onCompleteTask: (id: string, isChecked: boolean) => void;
+  onDeleteTask: (id: string) => void;
+}
+
+export const Task = ({ task, onCompleteTask, onDeleteTask }: TaskProps) => {
+  const [isChecked, setIsChecked] = useState(task.isCompleted);
 
   const handleTaskToggle = () => {
     setIsChecked(!isChecked);
+
+    onCompleteTask(task.id, !isChecked);
+  };
+
+  const handleDeleteTask = () => {
+    onDeleteTask(task.id);
   };
 
   const checkboxCheckedClassname = isChecked
@@ -19,25 +32,26 @@ export const Task = () => {
 
   return (
     <div className={styles.task}>
-      <div className={styles.checkboxContainer}>
-        <input readOnly type="checkbox" checked={isChecked} />
-        <span
-          onClick={handleTaskToggle}
-          className={`${styles.checkbox} ${checkboxCheckedClassname}`}
-        >
-          {isChecked && <Check size={12} />}
-        </span>
+      <div className={styles.content}>
+        <div className={styles.checkboxContainer}>
+          <input readOnly type="checkbox" checked={isChecked} />
+          <span
+            onClick={handleTaskToggle}
+            className={`${styles.checkbox} ${checkboxCheckedClassname}`}
+          >
+            {isChecked && <Check size={12} />}
+          </span>
+        </div>
+
+        <div className={styles.descriptionContainer}>
+          <p className={`${styles.description} ${paragraphCheckedClassname}`}>
+            {task.description}
+          </p>
+        </div>
       </div>
 
       <div>
-        <p className={`${styles.description} ${paragraphCheckedClassname}`}>
-          Integer urna interdum massa libero auctor neque turpis turpis semper.
-          Duis vel sed fames integer.
-        </p>
-      </div>
-
-      <div>
-        <button className={styles.deleteButton}>
+        <button className={styles.deleteButton} onClick={handleDeleteTask}>
           <Trash />
         </button>
       </div>
